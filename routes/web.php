d36498e410892a1ba96cf042243eb5a085e23f55
Pablo <pasabus45@gmail.com>
2025-11-26 23:22:03 -0600

    feat(routes): rutas agregadas para refacciones

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ServiciosController;
+use App\Http\Controllers\RefaccionesController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::resource('servicios', ServiciosController::class);
+Route::resource('refacciones', RefaccionesController::class);
